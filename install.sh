c2c36ae2f53feb8fc98f23ebda47212cf5edf2d7
Arawn <2300520400007@ietlucknow.ac.in>
2024-08-02 17:11:57 +0530

    Install.sh/Setup.py

---
+++ install.sh
@@ -0,0 +1,2 @@
+python setup.py bdist_wheel
+pip install .\dist\Quant_OptimalPortfolio-0.0.1-py3-none-any.whl --user --upgrade --force-reinstall
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Quant_OptimalPortfolio',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/Aditya-dom/Quant_OptimalPortfolio',
+    license='MIT',
+    author='Aditya',
+    author_email='domaditya10@gmail.com',
+    description='Portfolio Optimisation and Analytics Library',
+    # install_requires = ["numpy", "pandas", "scikit-learn", "scipy", "cvxpy"],
+    python_requires = ">=3.5",
+    project_urls = {
+    "Issues": "https://github.com/Aditya-dom/Quant_OptimalPortfolio/issues",
+    "Personal website": "https://arawn.live"}
+)
