f9d51c52642b18bfcdf7eae041a9537ae7ad8dc7
Arawn <2300520400007@ietlucknow.ac.in>
2024-08-03 11:07:45 +0530

    import error: Fix

--- tests/test_returns.py
+++ tests/test_returns.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import numpy as np 
-import optimalportfolio.returns as ret_estimators
+import numpy as np
+from build.lib.optimalportfolio import returns as ret_estimators
 from tests.test_utilities import get_asset_data, get_market_data
 
 asset_data = get_asset_data()
